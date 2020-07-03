1c06cd316701f377c10d0f05fef8ac17b3a2790f
CodeDoctorDE <proficodergames@gmail.com>
2020-07-03 14:47:47 +0200

    tictactoe update

--- src/main/java/com/github/codedoctorde/linwood/game/mode/tictactoe/TicTacToe.java
+++ src/main/java/com/github/codedoctorde/linwood/game/mode/tictactoe/TicTacToe.java
@@ -21,6 +21,18 @@ public class TicTacToe implements GameMode {
     private Game game;
     private TicTacToeEvents events;
     private long textChannelId;
+    /*
+    0\uFE0F\u20E3
+    1\uFE0F\u20E3
+    2\uFE0F\u20E3
+    3\uFE0F\u20E3
+    4\uFE0F\u20E3
+    5\uFE0F\u20E3
+    6\uFE0F\u20E3
+    7\uFE0F\u20E3
+    8\uFE0F\u20E3
+    9\uFE0F\u20E3
+     */
 
     public TicTacToe(int maxRounds, long rootChannel){
         this.maxRounds = maxRounds;
