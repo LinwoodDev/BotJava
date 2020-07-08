d9c9efcd3ba9213b7be4ea8d4e1f574bbc850199
CodeDoctorDE <wusel905@gmail.com>
2020-07-08 12:13:11 +0200

    add karma

---
+++ src/main/java/com/github/codedoctorde/linwood/commands/fun/KarmaCommand.java
@@ -0,0 +1,51 @@
+package com.github.codedoctorde.linwood.commands.fun;
+
+import com.github.codedoctorde.linwood.commands.Command;
+import com.github.codedoctorde.linwood.entity.GuildEntity;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
+import org.hibernate.Session;
+import org.jetbrains.annotations.Nullable;
+
+import java.awt.*;
+import java.time.LocalTime;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class KarmaCommand implements Command {
+    @Override
+    public boolean onCommand(Session session, Message message, GuildEntity entity, String label, String[] args) {
+        if(args.length != 0)
+        return false;
+        message.getChannel().sendMessage(" ").embed(new EmbedBuilder()
+                .setTitle("Karma")
+                .setDescription("Here you get all information to your karma statistics.")
+                .setColor(new Color(0x3B863B))
+                .setTimestamp(LocalTime.now())
+                .setAuthor(message.getAuthor().getAsTag(), "https://discordapp.com", message.getAuthor().getAvatarUrl())
+                .addField("Current karma points", "100", true)
+                .addField("Current dislikes", "5", true)
+                .addBlankField(false)
+                .addField("Current level", "10", true)
+                .addField("Experience", "10/20", true)
+                .addField("Rank", "#1", true)
+                .build()).queue();
+        return true;
+    }
+
+    @Override
+    public String[] aliases(GuildEntity entity) {
+        return new String[]{
+                "karma",
+                "likes",
+                "level",
+                "levels",
+                "rank"
+        };
+    }
+
+    @Override
+    public @Nullable ResourceBundle getBundle(GuildEntity entity) {
+        return ResourceBundle.getBundle("locale.commands.fun.Karma");
+    }
+}
