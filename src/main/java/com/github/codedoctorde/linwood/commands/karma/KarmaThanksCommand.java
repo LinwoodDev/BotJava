2fe89cdfc6d4e4ccb8d02998d2f892c75b4a9ad3
CodeDoctorDE <wusel905@gmail.com>
2020-08-28 17:17:30 +0200

    adding karma thanks command and translation

--- src/main/java/com/github/codedoctorde/linwood/commands/karma/KarmaThanksCommand.java
+++ src/main/java/com/github/codedoctorde/linwood/commands/karma/KarmaThanksCommand.java
@@ -2,26 +2,51 @@ package com.github.codedoctorde.linwood.commands.karma;
 
 import com.github.codedoctorde.linwood.commands.Command;
 import com.github.codedoctorde.linwood.entity.GuildEntity;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import org.hibernate.Session;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.ResourceBundle;
 import java.util.Set;
 
 public class KarmaThanksCommand implements Command {
     @Override
     public boolean onCommand(Session session, Message message, GuildEntity entity, String label, String[] args) {
-        return false;
+        if(args.length > 1)
+            return false;
+        var bundle = getBundle(entity);
+        var likeEmote = entity.getKarmaEntity().getLikeEmote();
+        if(likeEmote == null){
+            message.getChannel().sendMessage(bundle.getString("NoSetup")).queue();
+            return true;
+        }
+        if(args.length == 0){
+            message.getChannel().sendMessageFormat(bundle.getString("ThanksSimple"), likeEmote).queue();
+        }else{
+            message.getChannel().retrieveMessageById(args[0]).submit().whenComplete((mentionedMessage, throwable) -> {
+                if(throwable != null)
+                   message.getChannel().sendMessage(bundle.getString("NotFound")).queue();
+               else {
+                   mentionedMessage.addReaction(entity.getKarmaEntity().getLikeEmote()).queue();
+                   message.getChannel().sendMessage(new EmbedBuilder().setTitle(bundle.getString("Title"))
+                           .setAuthor(mentionedMessage.getAuthor().getAsTag(), "https://discord.com", mentionedMessage.getAuthor().getAvatarUrl()).setDescription(mentionedMessage.getContentRaw())
+                           .addField(bundle.getString("JumpHeader"), String.format(bundle.getString("JumpBody"), entity.getKarmaEntity().getLikeEmote(), mentionedMessage.getJumpUrl()), false).build()).queue();
+               }});
+        }
+        return true;
     }
 
     @Override
     public @NotNull Set<String> aliases(GuildEntity entity) {
-        return null;
+        return new HashSet<>(Arrays.asList("t", "thanks"));
     }
 
     @Override
     public @NotNull ResourceBundle getBundle(GuildEntity entity) {
-        return null;
+        return ResourceBundle.getBundle("locale.commands.karma.Thanks");
     }
 }
